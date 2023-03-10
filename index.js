a045f10fb6e0fae94e0f8ff8ea027a5c2ab7a5db
kesquerra <kyle.esquerra.dev@gmail.com>
2023-03-10 00:14:16 -0800

    add resume data json, add functions to generate resume, add resume html template

--- index.js
+++ index.js
@@ -48,5 +48,90 @@ async function getJson(file) {
     return await fetch(file).then(resp => resp.json())
 }
 
+async function createResume() {
+    createWorkExperience();
+    createEducation();
+}
 
+async function createEducation() {
+    await getJson("./data/education.json").then(json => {
+        let ul = $("<ul/>");
+        json.forEach(degree => {
+            let deg_div = $("<div/>", {
+                class: "horiz"
+            })
+            let honors = degree.honors
+            if (honors == undefined || honors == null) {
+                honors = ""
+            } else {
+                honors = ", " + honors;
+            }
+            deg_div.append(
+                $("<span/>", {
+                    text: degree.type + " in " + degree.concentration +
+                    ", " + degree.institution + honors
+                })
+            )
+            deg_div.append(
+                $("<span/>", {
+                    text: degree.graduation_year
+                })
+            )
+            
+            ul.append(deg_div);
+        })
+        $("#education").append(ul);
+
+    })
+}
+
+async function createWorkExperience() {
+    await getJson("./data/prev_work.json").then(json => {
+        let work = $("#work");
+        json.reverse().forEach(employer => {
+            let emp_div = $("<div/>", {
+                class: "employer"
+            })
+            let emp_label = $("<div/>", {
+                class: "horiz bold"
+            })
+            emp_label.append($("<span/>", {
+                text: employer.employer
+            }))
+            emp_label.append($("<span/>", {
+                text: employer.years
+            }))
+            emp_div.append(emp_label);
+            let positions_div = ($("<ul/>", {
+                class: "positions",
+            }))
+            emp_div.append(positions_div);
+            employer.positions.reverse().forEach(position => {
+                let pos_div = $("<div/>", {
+                    class: "position"
+                })
+                let pos_label = $("<div/>", {
+                    class: "horiz italic"
+                })
+                pos_label.append($("<span/>", {
+                    text: position.title
+                }))
+                pos_label.append($("<span/>", {
+                    text: position.years
+                }))
+                pos_div.append(pos_label)
+                let desc_div = ($("<ul/>", {
+                    class: "descriptors"
+                })).appendTo(pos_div);
+                position.descriptors.forEach(desc => {
+                    desc_div.append($("<li/>", {
+                        text: desc
+                    }))
+                })
+                positions_div.append(pos_div);
+            })
+            work.append(emp_div);
+        });
+    });
+}
 
